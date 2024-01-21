fb1688949c7a5e356a3e3571d8b1be7b303030fc
RathiPM <rathipriyap.21ece@kongu.edu>
2024-01-21 22:49:00 +0530

    rathi

---
+++ operators.js
@@ -0,0 +1,33 @@
+//Assignment Operator
+var a=10
+console.log("Assignment Operator Output: ",a)
+
+//Arithemetic Operator
+var aA=10
+var bA=10
+console.log(typeof aA)
+console.log("Arithemetic Operator Addition Output: ",aA+bA)
+console.log("Arithemetic Operator Subtraction Output: ",aA-bA)
+console.log("Arithemetic Operator Multiplication Output: ",aA*bA)
+console.log("Arithemetic Operator Division Output: ",aA/bA)
+console.log("Arithemetic Operator Modulus Output: ",aA%bA)
+
+
+//Logical Operator --> &&, ||, != =>Output
+var aA=10
+var bA=10
+console.log("Logical Operator && output:",aA&&bA)
+console.log("Logical Operator || output:",aA||bA)
+console.log("Logical Operator ! output:",!aA)
+
+
+console.log(10 && 20)
+console.log(10 && 30)
+console.log(10 && 0)
+console.log(0 && 30)
+
+
+console.log(10 || 20)
+console.log(0 || 20)
+console.log(20 || 0)
+console.log(100 || 20 && 0)
