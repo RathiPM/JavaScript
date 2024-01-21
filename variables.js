60fbdbede268aa8e160a7f354bc5c0adf3ee7d7a
RathiPM <rathipriyap.21ece@kongu.edu>
2024-01-21 22:46:53 +0530

    rathi

---
+++ variables.js
@@ -0,0 +1,8 @@
+a='a'
+console.log(a)
+var b='var'
+console.log("This variabe belongs to var type",b)
+let c='let'
+console.log("This variabe belongs to var type",c)
+const d=100
+console.log("This variabe belongs to var type",d)
